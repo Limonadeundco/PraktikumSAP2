51f04c70d08924873b3d6c7e43d36639d45e61f9
Limonadeundco <emildess0303@gymhelm.onmicrosoft.com>
2024-01-15 15:35:24 +0100

    Add database_commands import and refactor code for recommended_product_functions

--- Recommended_product_functions.py
+++ Recommended_product_functions.py
@@ -1,8 +1,9 @@
 import datetime
+import database_commands
 
 class recommended_product_functions():
     def __init__(self):
-        pass
+        self.dataBase = database_commands.DataBase()
     
     def generate_recommended_products(self, connection, cursor, product_count):
         cursor.execute("SELECT id FROM products")
@@ -10,15 +11,17 @@ class recommended_product_functions():
         
         products_sales = []
         for product_id in dataBase_response:
-            sales_last_day = self.get_sales_last_day(connection, cursor, product_id)
+            sales_last_day = self.get_sales_last_day(connection, cursor, product_id[0])
+            #print("sales_last_day: ", sales_last_day)
             products_sales.append([product_id, sales_last_day])
             
         products_sales.sort(key=lambda x: x[1], reverse=True)
         
         for i in range(product_count):
-            product_id = products_sales[i][0]
+            product_id = products_sales[i][0][0]
             product_sales = products_sales[i][1]
-            self.insert_data(connection, cursor, "recommended_products", "product_id, sales_last_day", (product_id, product_sales,))
+            #print("product_id: ", product_id, "product_sales: ", product_sales)
+            self.dataBase.insert_data(connection, cursor, "recommended_products", "product_id, sales_last_day", (product_id, product_sales,))
             
         
     def get_sales_last_day(self, connection, cursor, product_id):
@@ -28,10 +31,14 @@ class recommended_product_functions():
         # Calculate the time 24 hours ago
         last_day_time = current_time - datetime.timedelta(days=1)
 
+        # Format the last_day_time to a string
+        last_day_time_str = last_day_time.strftime('%Y-%m-%d %H:%M:%S')
+
         # Execute SQL query to get sales from the last 24 hours
-        cursor.execute(f"SELECT id FROM sales WHERE sale_time >= {last_day_time} AND product_id = {product_id}")
+        cursor.execute(f"SELECT id FROM sales WHERE sale_time >= '{last_day_time_str}' AND product_id = '{product_id}'")
         dataBase_response = cursor.fetchall()
-        
+
         sales_last_day = len(dataBase_response)
-        
+        #print("sales_last_day: ", sales_last_day)
+
         return sales_last_day
--- test_recommended_product_functions.py
+++ test_recommended_product_functions.py
@@ -1,32 +1,110 @@
 import unittest
 from recommended_product_functions import *
-from unittest.mock import MagicMock
-from unittest.mock import patch
-from unittest.mock import Mock
+import os
+import sqlite3
+import database_commands
+
 
 class TestRecommendedProductFunctions(unittest.TestCase):
     
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.recommended_product_functions = recommended_product_functions()
-        self.connection = Mock()
-        self.cursor = Mock()
+        self.connection = sqlite3.connect("TestRecommendedProductFunctions.db")
+        self.cursor = self.connection.cursor()
+        self.dataBase = database_commands.DataBase()
+        
+        self.dataBase.create_table(self.connection, self.cursor, "products", "id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, description TEXT, count INTEGER, sales INTEGER")
+        
+        self.dataBase.create_table(self.connection, self.cursor, "sales", "id INTEGER PRIMARY KEY AUTOINCREMENT, product_id INTEGER, sale_time TEXT, count INTEGER")
+        
+        self.dataBase.create_table(self.connection, self.cursor, "recommended_products", "id INTEGER PRIMARY KEY AUTOINCREMENT, product_id INTEGER, sales_last_day INTEGER")
+        
+    @classmethod
+    def tearDownClass(self):
+        self.connection.close()
+        os.remove("TestRecommendedProductFunctions.db")
+        
+    
+    def setUp(self):
+        #clear tables
+        self.dataBase.clear_table(self.connection, self.cursor, "products")
+        self.dataBase.clear_table(self.connection, self.cursor, "sales")
+        self.dataBase.clear_table(self.connection, self.cursor, "recommended_products")
+        
+        #generate test data
+        self.dataBase.insert_data(self.connection, self.cursor, "products", "name, price, description, count, sales", ("test_product_1", 100, "test_description_1", 10, 0,))
+        self.dataBase.insert_data(self.connection, self.cursor, "products", "name, price, description, count, sales", ("test_product_2", 200, "test_description_2", 20, 0,))
+        self.dataBase.insert_data(self.connection, self.cursor, "products", "name, price, description, count, sales", ("test_product_3", 300, "test_description_3", 30, 0,))
+        self.dataBase.insert_data(self.connection, self.cursor, "products", "name, price, description, count, sales", ("test_product_4", 400, "test_description_4", 40, 0,))
+        self.dataBase.insert_data(self.connection, self.cursor, "products", "name, price, description, count, sales", ("test_product_5", 500, "test_description_5", 50, 0,))
+        
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (1, "2024-01-15 00:10:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (1, "2024-01-15 00:30:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (1, "2024-01-15 00:50:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (1, "2024-01-15 00:00:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (2, "2024-01-15 00:20:00", 1,))
+        
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (2, "2024-01-15 00:30:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (2, "2024-01-15 00:50:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (2, "2024-01-15 00:40:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (3, "2024-01-15 00:00:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (3, "2024-01-15 00:20:00", 1,))
+        
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (3, "2024-01-15 00:10:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (4, "2024-01-15 00:40:00", 1,))
+        self.dataBase.insert_data(self.connection, self.cursor, "sales", "product_id, sale_time, count", (4, "2024-01-15 00:30:00", 1,))
         
     def test_generate_recommended_products(self):
-        self.cursor.fetchall.return_value = [[1], [2], [3]]
-        self.recommended_product_functions.get_sales_last_day = MagicMock(return_value=1)
-        self.recommended_product_functions.insert_data = MagicMock()
+        self.recommended_product_functions.generate_recommended_products(self.connection, self.cursor, 5)
         
-        self.recommended_product_functions.generate_recommended_products(self.connection, self.cursor, 3)
+        self.cursor.execute("SELECT * FROM recommended_products")
+        dataBase_response = self.cursor.fetchall()
         
-        self.recommended_product_functions.get_sales_last_day.assert_called()
-        self.recommended_product_functions.insert_data.assert_called()
+        self.assertEqual(dataBase_response[0][1], 1)
+        self.assertEqual(dataBase_response[1][1], 2)
+        self.assertEqual(dataBase_response[2][1], 3)
+        self.assertEqual(dataBase_response[3][1], 4)
+        self.assertEqual(dataBase_response[4][1], 5)
+        
+        #test if the sales_last_day is correct
+        self.cursor.execute("SELECT * FROM recommended_products WHERE product_id = 1")
+        dataBase_response = self.cursor.fetchall()
+        self.assertEqual(dataBase_response[0][2], 4)
+        self.cursor.execute("SELECT * FROM recommended_products WHERE product_id = 2")
+        dataBase_response = self.cursor.fetchall()
+        self.assertEqual(dataBase_response[0][2], 4)
+        self.cursor.execute("SELECT * FROM recommended_products WHERE product_id = 3")
+        dataBase_response = self.cursor.fetchall()
+        self.assertEqual(dataBase_response[0][2], 3)
+        
+        #check if the id is unigue
+        self.cursor.execute("SELECT * FROM recommended_products")
+        dataBase_response = self.cursor.fetchall()
+        self.assertEqual(dataBase_response[0][0], 1)
+        self.assertEqual(dataBase_response[1][0], 2)
+        self.assertEqual(dataBase_response[2][0], 3)
+        self.assertEqual(dataBase_response[3][0], 4)
+        
+        #print(dataBase_response)
+        
+
         
     def test_get_sales_last_day(self):
-        self.cursor.fetchall.return_value = [[1], [2], [3]]
+        sales_last_day = self.recommended_product_functions.get_sales_last_day(self.connection, self.cursor, 1)
+        self.assertEqual(sales_last_day, 4)
         
-        self.recommended_product_functions.get_sales_last_day(self.connection, self.cursor, 1)
+        sales_last_day = self.recommended_product_functions.get_sales_last_day(self.connection, self.cursor, 2)
+        self.assertEqual(sales_last_day, 4)
         
-        self.cursor.execute.assert_called()
+        sales_last_day = self.recommended_product_functions.get_sales_last_day(self.connection, self.cursor, 3)
+        self.assertEqual(sales_last_day, 3)
+        
+        sales_last_day = self.recommended_product_functions.get_sales_last_day(self.connection, self.cursor, 4)
+        self.assertEqual(sales_last_day, 2)
+        
+        sales_last_day = self.recommended_product_functions.get_sales_last_day(self.connection, self.cursor, 5)
+        self.assertEqual(sales_last_day, 0)
         
         
 if __name__ == '__main__':
