be5b25b733518524a219bc623582073bfc3e068c
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-01-15 10:03:16 +0000

    Add API endpoints to get products by ID and all products

--- flask_server.py
+++ flask_server.py
@@ -10,8 +10,19 @@ class Server():
         dataBase.create_table(connection, cursor, "products", "id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, description TEXT, count INTEGER")
         dataBase.insert_data(connection, cursor, "products", "name, price, description, count", ("Test", 10.0, "Test", 10))
         
+        
+    ################################################################
+    #                                                              #    
+    #               Get products for id                            #
+    #                                                              #
+    ################################################################
     @app.route("/get_product/<product_id>/<column>", methods=["GET"])
     def get_data(product_id, column):
+        try:
+            product_id = int(product_id)
+        except ValueError:
+            return flask.Response("Invalid id", status=404)
+        
         print(column)
         if column not in ["name", "price", "description", "count"]:
             return flask.Response("Column not found", status=404)
@@ -20,20 +31,53 @@ class Server():
         
         database_response = dataBase.select_data(cursor, "products", column, f"id = {product_id}")
         
-        if database_response is None:
-            return flask.Response("Product or column not found", status=199)
+        if database_response == []:
+            return flask.Response("Product not found", status=404)
         
         #format response nicely into a good looking json
         return flask.jsonify(database_response)
     
     @app.route("/get_product/<product_id>", methods=["GET"])
     def get_product(product_id):
+        try:
+            product_id = int(product_id)
+        except ValueError:
+            return flask.Response("Invalid id", status=404)
+        
         _, cursor = dataBase.connect_database("database.db")
         
         database_response = dataBase.select_data(cursor, "products", "*", f"id = {product_id}")
         
+        if database_response == []:
+            return flask.Response("Product not found", status=404)
+        
+        return flask.jsonify(database_response)
+    
+    
+    @app.route("/get_all_products/<limit>", methods=["GET"])
+    def get_all_products(limit):
+        try:
+            limit = int(limit)
+        except ValueError:
+            return flask.Response("Invalid limit", status=404)
+        
+        _, cursor = dataBase.connect_database("database.db")
+        
+        database_response = dataBase.select_data(cursor, "products", "*", f"id < {limit + 1}")
+        
         if database_response is None:
-            return flask.Response("Product or column not found", status=199)
+            return flask.Response("No Products found", status=404)
+        
+        return flask.jsonify(database_response)
+    
+    @app.route("/get_all_products", methods=["GET"])
+    def get_all_products_no_limit():
+        _, cursor = dataBase.connect_database("database.db")
+        
+        database_response = dataBase.select_data(cursor, "products", "*", "1")
+        
+        if database_response is None:
+            return flask.Response("No Products found", status=404)
         
         return flask.jsonify(database_response)
     
