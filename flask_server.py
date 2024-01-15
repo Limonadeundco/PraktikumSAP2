a25d397b9ec63e6112f36cfdaec81222aad568e8
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-01-15 08:46:01 +0000

    Add Flask server routes for inserting and retrieving data

--- flask_server.py
+++ flask_server.py
@@ -1,2 +1,15 @@
 import flask
 from database_commands import *
+
+dataBase = DataBase()
+app = flask.Flask(__name__)
+
+class Server():
+    @app.route("/insert_data/<table>/<columns>/<data>", methods=["POST"])   
+    def insert_data(table, columns, data):
+        dataBase.insert_data(dataBase.connect_database("database.db"), table, columns, (data,))
+        return "Data inserted"
+    
+    @app.route("/get_data/<table>/<columns>/<condition>", methods=["GET"])
+    def get_data(table, columns, condition):
+        return str(dataBase.select_data(dataBase.connect_database("database.db"), table, columns, condition))
