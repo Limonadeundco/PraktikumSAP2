ba6ea670e93b3ae40901b35aae77e39e06bc7123
Limonadeundco <desslochemil@gmail.com>
2024-01-15 12:42:15 +0100

    Update request_client.py to use the correct ID for updating a product

--- request_client.py
+++ request_client.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://127.0.0.1:5000/update_product/1/name=updated_name"
+url = "http://127.0.0.1:5000/update_product/1/id=3"
 
 response = requests.put(url)
 
