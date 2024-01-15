f4270ef1d34bbbf9b2f75414311f292074eef280
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-01-15 09:42:11 +0000

    Refactor database connection and add new API endpoints

--- flask_server.py
+++ flask_server.py
@@ -6,17 +6,37 @@ app = flask.Flask(__name__)
 
 class Server():
     def __init__(self):
-        cursor, connection = dataBase.connect_database("database.db")
-        dataBase.create_table(cursor, connection, "products", "name TEXT, price REAL, description TEXT, count INTEGER")
+        connection, cursor = dataBase.connect_database("database.db")
+        dataBase.create_table(connection, cursor, "products", "id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, description TEXT, count INTEGER")
+        dataBase.insert_data(connection, cursor, "products", "name, price, description, count", ("Test", 10.0, "Test", 10))
         
     @app.route("/get_product/<product_id>/<column>", methods=["GET"])
     def get_data(product_id, column):
+        print(column)
         if column not in ["name", "price", "description", "count"]:
             return flask.Response("Column not found", status=404)
         
         _, cursor = dataBase.connect_database("database.db")
         
-        return str(dataBase.select_data(cursor, "products", column, f"id = {product_id}"))
+        database_response = dataBase.select_data(cursor, "products", column, f"id = {product_id}")
+        
+        if database_response is None:
+            return flask.Response("Product or column not found", status=199)
+        
+        #format response nicely into a good looking json
+        return flask.jsonify(database_response)
+    
+    @app.route("/get_product/<product_id>", methods=["GET"])
+    def get_product(product_id):
+        _, cursor = dataBase.connect_database("database.db")
+        
+        database_response = dataBase.select_data(cursor, "products", "*", f"id = {product_id}")
+        
+        if database_response is None:
+            return flask.Response("Product or column not found", status=199)
+        
+        return flask.jsonify(database_response)
     
 if __name__ == "__main__":
+    Server = Server()
     app.run(debug=True)
