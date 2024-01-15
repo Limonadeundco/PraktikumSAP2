0f53b14b8c135fecb243045f0e150c2eb919fe32
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-01-15 10:38:17 +0100

    Add test cases for get_product endpoint

--- test_flask_server.py
+++ test_flask_server.py
@@ -51,6 +51,29 @@ class TestFlaskServer(unittest.TestCase):
             raise
         self.assertEqual(response.status_code, 200)
         
+        response = self.app.get("/get_product/1/invalid_column")
+        try:
+            self.assertEqual(response.data, b"Column not found")
+        except AssertionError:
+            print("Unexpected response data:", response.data)
+            raise
+        self.assertEqual(response.status_code, 404)
+        
+        response = self.app.get("/get_product/invalid_id/count")
+        try:
+            self.assertEqual(response.data, b"Data not found")
+        except AssertionError:
+            print("Unexpected response data:", response.data)
+            raise
+        self.assertEqual(response.status_code, 404)
+        
+        response = self.app.get("/get_product/999/count")
+        try:
+            self.assertEqual(response.data, b"Data not found")
+        except AssertionError:
+            print("Unexpected response data:", response.data)
+            raise
+        
     def test_recommended_products(self):
         # recommended products are products with the highest count
         database_commands.DataBase().insert_data(self.conn, self.cursor, "products", "name, price, description, count", ("test_data", 1.0, "test_data_desc", 1))
@@ -89,6 +112,27 @@ class TestFlaskServer(unittest.TestCase):
         response = self.app.get("/get_all_products")
         try:
             self.assertEqual(response.data, b"[('test_data',), ('test_data4',), ('test_data2',), ('test_data3',), ('test_data5',)]")
+        except AssertionError:
+            print("Unexpected response data:", response.data)
+            raise
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_product(self):
+        database_commands.DataBase().insert_data(self.conn, self.cursor, "products", "name, price, description, count", ("test_data", 1.0, "test_data_desc", 1))
+        database_commands.DataBase().insert_data(self.conn, self.cursor, "products", "name, price, description, count", ("test_data4", 1.0, "test_data_desc4", 4))
+        database_commands.DataBase().insert_data(self.conn, self.cursor, "products", "name, price, description, count", ("test_data2", 1.0, "test_data_desc2", 2))
+        database_commands.DataBase().insert_data(self.conn, self.cursor, "products", "name, price, description, count", ("test_data3", 1.0, "test_data_desc3", 3))
+        database_commands.DataBase().insert_data(self.conn, self.cursor, "products", "name, price, description, count", ("test_data5", 1.0, "test_data_desc5", 5))
+        
+        database_commands.DataBase().update_data(self.conn, self.cursor, "products", "count = 1, price = 1.0, name = 'test_data', description = 'test_data_desc'", "id = 1")
+        database_commands.DataBase().update_data(self.conn, self.cursor, "products", "count = 4, price = 1.0, name = 'test_data4', description = 'test_data_desc4'", "id = 2")
+        database_commands.DataBase().update_data(self.conn, self.cursor, "products", "count = 2, price = 1.0, name = 'test_data2', description = 'test_data_desc2'", "id = 3")
+        database_commands.DataBase().update_data(self.conn, self.cursor, "products", "count = 3, price = 1.0, name = 'test_data3', description = 'test_data_desc3'", "id = 4")
+        database_commands.DataBase().update_data(self.conn, self.cursor, "products", "count = 5, price = 1.0, name = 'test_data5', description = 'test_data_desc5'", "id = 5")
+        
+        response = self.app.get("/get_product/1")
+        try:
+            self.assertEqual(response.data, b"[('test_data', 1.0, 'test_data_desc', 1)]")
         except AssertionError:
             print("Unexpected response data:", response.data)
             raise
