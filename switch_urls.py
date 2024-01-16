34f0f62110194ec6e67543c5092cbe7bea8ee296
Limonadeundco <emildess0303@gymhelm.onmicrosoft.com>
2024-01-16 15:43:19 +0100

    Refactor URL replacement logic in switch_urls.py

--- switch_urls.py
+++ switch_urls.py
@@ -1,17 +1,24 @@
 import os
+import re
 
-def replace_url(folder_path, old_url, new_url):
+def replace_url(old_url, new_url, reverse=False):
+    if reverse:
+        old_url, new_url = new_url, old_url
+    folder_path = os.path.dirname(os.path.realpath(__file__))
+    print(folder_path)
     for root, dirs, files in os.walk(folder_path):
         for file in files:
-            if file.endswith(('.html', '.css', '.js')):
+            if file.endswith(('html', 'js', 'css')):
                 file_path = os.path.join(root, file)
-                #read lines and replace old_url with new_url
+                print("file: ", file)
                 with open(file_path, 'r') as f:
-                    lines = f.readlines()
+                    content = f.read()
                 
+                new_content = re.sub(re.escape(old_url) + r'(?=/)', new_url, content)
+
                 with open(file_path, 'w') as f:
-                    for line in lines:
-                        f.write(line.replace(old_url, new_url))    
+                    f.write(new_content)
 
 # Usage example
-replace_url('/', 'http://127.0.0.1:5000', 'https://silver-goldfish-7xg65j5rx5xhww4g-5500.app.github.dev/')
+replace_url('http://127.0.0.1:5500', 'https://silver-goldfish-7xg65j5rx5xhww4g-5500.app.github.dev', False)
+#replace_url('https://silver-goldfish-7xg65j5rx5xhww4g-5500.app.github.dev', 'http://127.0.0.1:5500', True)
