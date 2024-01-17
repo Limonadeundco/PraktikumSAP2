1c49ff1fa815e0225131ab87c1d001479da043c7
Limonadeundco <desslochemil+github@gmail.com>
2024-01-17 10:35:18 +0100

    Refactor get_cookie route to handle existing cookies

--- flask_server.py
+++ flask_server.py
@@ -422,14 +422,36 @@ class Server():
     #                                                              #
     ################################################################
     @app.route("/get_cookie", methods=["GET"])
-    def get_cookie():
-        # Generate a new user id
-        user_id = str(uuid.uuid4())
-
-        # Save the user id to the database
+    @app.route("/get_cookie/<uuid>", methods=["GET"])
+    def get_cookie(uuid=None):
         connection, cursor = dataBase.connect_database("database.db")
-        dataBase.insert_data(connection, cursor, "cookies", "cookie_id", (user_id,))
+        if uuid is None:
 
+            while True:
+                # Generate a new user id
+                user_id = str(uuid.uuid4())
+
+                # Check if the user id is already in the database
+                cursor.execute("SELECT * FROM cookies WHERE cookie_id = ?", (user_id,))
+                row = cursor.fetchone()
+
+                # If the user id is not in the database, break the loop
+                if row is None:
+                    break
+
+            # Save the new user id to the database
+            dataBase.insert_data(connection, cursor, "cookies", "cookie_id", (user_id,))
+        else:
+            cursor.execute("SELECT * FROM cookies WHERE cookie_id = ?", (uuid,))
+            row = cursor.fetchone()
+
+            # If the user id is not in the database, break the loop
+            if row != None:
+                return flask.Response("Cookie already exists", status=404)
+            else:
+                user_id = uuid
+                dataBase.insert_data(connection, cursor, "cookies", "cookie_id", (user_id,))
+        
         # Return the new user id
         return flask.jsonify(user_id=user_id)
     
