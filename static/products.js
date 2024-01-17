a9a4750276e030dab50f8e307def8aa4e2a44592
EinEisbxr <flixe0001@gmail.com>
2024-01-17 15:51:03 +0100

    Refactor database response and product rendering

--- static/products.js
+++ static/products.js
@@ -1,5 +1,6 @@
 var products_per_row = 5;
 var rows = 10;
+var search_results = null;
 
 async function httpGetJson(url) {
     return fetch(url)
@@ -16,6 +17,21 @@ async function httpGetJson(url) {
         });
 }
 
+async function httpGetText(url) {
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .catch((e) => {
+            console.log(
+                "There was a problem with your fetch operation: " + e.message
+            );
+        });
+}
+
 async function getImportantData() {
     let product_count = await httpGetJson(
         "http://127.0.0.1:5000/get_number_of_all_products"
@@ -24,71 +40,73 @@ async function getImportantData() {
         "http://127.0.0.1:5000/get_all_products/" + rows * products_per_row
     );
 
-    return [product_count, get_all_products];
+    return { productCount: product_count[0][0], allProducts: get_all_products };
 }
 
-async function generateTable(data) {
-    let product_count = await httpGetJson(
-        "http://127.0.0.1:5000/get_product_count"
-    );
+async function generateTable(initData, tableData) {
+    let product_count = initData.productCount;
+
     if (product_count > rows * products_per_row) {
         product_count = rows * products_per_row;
     }
 
-    let table = document.createElement("table");
-    table.classList.add("table");
-    table.classList.add("table-striped");
-    table.classList.add("table-hover");
+    let table = document.getElementById("products");
+    let table_body = table.getElementsByTagName("tbody")[0];
 
-    let thead = document.createElement("thead");
-    let tr = document.createElement("tr");
-    let th = document.createElement("th");
-    th.setAttribute("scope", "col");
-    th.innerText = "Products found";
-    tr.appendChild(th);
+    let products_found = document.getElementById("product-count-thead");
+    products_found.innerHTML = product_count + " Produkte gefunden";
 
-    thead.appendChild(tr);
-    table.appendChild(thead);
+    for (let i = 0; i < tableData.length; i++) {
+        let row = document.createElement("tr");
+        for (let y = 0; y < tableData[i].length; y++) {
+            let product = tableData[i][y];
 
-    let tbody = document.createElement("tbody");
-    for (let i = 0; i < data.length; i++) {
-        let row = data[i];
-        tr = document.createElement("tr");
-        let td = document.createElement("td");
-        td.innerText = row[0];
-        tr.appendChild(td);
+            if (product == null) {
+                break;
+            }
 
-        td = document.createElement("td");
-        td.innerText = row[1];
-        tr.appendChild(td);
+            product = product.product;
 
-        td = document.createElement("td");
-        td.innerText = row[2];
-        tr.appendChild(td);
+            let product_container = document.createElement("td");
+            product_container.classList.add("product-container");
 
-        td = document.createElement("td");
-        td.innerText = row[3];
-        tr.appendChild(td);
+            let product_image = document.createElement("img");
+            product_image.src = "http://127.0.0.1:5000/get_image/" + product.id;
+            product_image.alt = product.product_name;
+            product_image.classList.add("product-image");
+            product_container.appendChild(product_image);
 
-        tbody.appendChild(tr);
+            let product_name = document.createElement("p");
+            product_name.innerHTML = product.name;
+            product_name.classList.add("product-name");
+            product_container.appendChild(product_name);
+
+            let product_price = document.createElement("p");
+            product_price.innerHTML = product.price + "€";
+            product_price.classList.add("product-price");
+            product_container.appendChild(product_price);
+
+            let product_description = document.createElement("p");
+            product_description.innerHTML = product.description;
+            product_description.classList.add("product-description");
+            product_container.appendChild(product_description);
+
+            row.appendChild(product_container);
+        }
+        table_body.appendChild(row);
     }
-    table.appendChild(tbody);
-
-    return table;
 }
 
-async function calculateTable() {
+async function calculateTable(initData) {
     let table = [];
 
-    let result = await httpGetJson(
-        "http://127.0.0.1:5000/get_all_products/" + rows * products_per_row
-    );
-    result = result.products;
+    let products = initData.allProducts.products;
+    console.log(products);
 
     for (let i = 0; i < rows; i++) {
         let row = [];
         for (let j = 0; j < products_per_row; j++) {
-            row.push(result[i * products_per_row + j]);
+            row.push(products[i * products_per_row + j]);
         }
         table.push(row);
     }
@@ -96,11 +114,51 @@ async function calculateTable() {
     return table;
 }
 
-window.addEventListener("load", function () {
-    let importantData = getImportantData();
-    console.log(importantData);
+async function deleteTable() {
+    let table = document.getElementById("products");
+    let table_body = table.getElementsByTagName("tbody")[0];
 
-    calculateTable().then((table) => {
+    while (table_body.firstChild) {
+        table_body.removeChild(table_body.firstChild);
+    }
+}
+
+async function searchProducts() {
+    let search_input = document.getElementById("search").value;
+    search_results = await httpGetJson(
+        "http://127.0.0.1:5000/search/" + search_input
+    );
+
+    let product_count = await httpGetJson(
+        "http://127.0.0.1:5000/get_number_of_all_products"
+    );
+
+    return { productCount: product_count[0][0], allProducts: search_results };
+}
+
+async function onSearch() {
+    let search_input = document.getElementById("search").value;
+
+    if (search_input.length < 3) {
+        initData = await getImportantData();
+    } else {
+        initData = await searchProducts();
+    }
+
+    deleteTable();
+    calculateTable(initData).then((table) => {
+        generateTable(initData, table).then((table) => {});
+    });
+}
+
+window.addEventListener("load", async function () {
+    initData = await getImportantData();
+    console.log(initData);
+
+    calculateTable(initData).then((table) => {
         console.log(table);
+        generateTable(initData, table).then((table) => {
+            console.log(table);
+        });
     });
 });
